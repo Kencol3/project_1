7c03debc72b13007e3cdf260f6d053360cf82968
Kencol3 <ken.colangelo@gmail.com>
2018-01-09 15:38:38 -0500

    modified

--- draw.py
+++ draw.py
@@ -3,9 +3,9 @@ import turtle
 
 painter = turtle.Turtle()
 
-for _ in range(4):
-	painter.forward(11)
-	painter.left(90)
+for _ in range(18):
+	painter.forward(100)
+	painter.left(100)
 
 turtle.done()
 
